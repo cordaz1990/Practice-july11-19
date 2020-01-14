7a7389dc4960a43c2ce7746893220abd56336eaf
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 20:01:21 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,19 @@
-print('hello')
-print('world'
+#Boolean Funtions 
+
+def is_divisible(x,y):
+    if x % y == 0:
+        return True
+    else:
+      return False
+    
+is_divisible(6,4) #-->False
+is_divisible(6,3) #--True
+
+def is_divisible(x,y):
+    return x % y == 0
+  
+    if is_divisible(x,y):
+       print('x is divisible by y')
+        
+    if is_divisible(x,y) == True:
+       print('x is divisible by y')
